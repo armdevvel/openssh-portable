6ae57ab6ddfa51d49c033a405cea892c5420ccdc
treeswift <treeswift@mailbox.org>
2023-10-05 07:20:36 -0500

    pull [un]set_nonblock into openbsd_compat

--- misc.c
+++ misc.c
@@ -107,55 +107,6 @@ rtrim(char *s)
 	}
 }
 
-/* set/unset filedescriptor to non-blocking */
-int
-set_nonblock(int fd)
-{
-	int val;
-
-	val = fcntl(fd, F_GETFL);
-	if (val == -1) {
-		error("fcntl(%d, F_GETFL): %s", fd, strerror(errno));
-		return (-1);
-	}
-	if (val & O_NONBLOCK) {
-		debug3("fd %d is O_NONBLOCK", fd);
-		return (0);
-	}
-	debug2("fd %d setting O_NONBLOCK", fd);
-	val |= O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, val) == -1) {
-		debug("fcntl(%d, F_SETFL, O_NONBLOCK): %s", fd,
-		    strerror(errno));
-		return (-1);
-	}
-	return (0);
-}
-
-int
-unset_nonblock(int fd)
-{
-	int val;
-
-	val = fcntl(fd, F_GETFL);
-	if (val == -1) {
-		error("fcntl(%d, F_GETFL): %s", fd, strerror(errno));
-		return (-1);
-	}
-	if (!(val & O_NONBLOCK)) {
-		debug3("fd %d is not O_NONBLOCK", fd);
-		return (0);
-	}
-	debug("fd %d clearing O_NONBLOCK", fd);
-	val &= ~O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, val) == -1) {
-		debug("fcntl(%d, F_SETFL, ~O_NONBLOCK): %s",
-		    fd, strerror(errno));
-		return (-1);
-	}
-	return (0);
-}
-
 const char *
 ssh_gai_strerror(int gaierr)
 {
