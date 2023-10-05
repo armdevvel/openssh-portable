0085355455d14ea7ffe6195eaf624fecbabd4470
treeswift <treeswift@mailbox.org>
2023-10-05 03:59:56 -0500

    make the compat layer log to stderr, rendering it app-independent

---
+++ contrib/win32/win32compat/stderrlog.c
@@ -0,0 +1,45 @@
+/* public domain */
+/* follows fatal.c from sk-dummy */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include "log.h"
+
+void
+sshlog(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *suffix, const char *fmt, ...)
+{
+	va_list ap;
+
+    if(level <= STDERR_LOG_LEVEL)
+    {
+        if (showfunc)
+            fprintf(stderr, "%s: ", func);
+        va_start(ap, fmt);
+        vfprintf(stderr, fmt, ap);
+        va_end(ap);
+        if (suffix != NULL)
+            fprintf(stderr, ": %s", suffix);
+        fputc('\n', stderr);
+    }
+}
+
+void
+sshfatal(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *suffix, const char *fmt, ...)
+{
+	va_list ap;
+
+	if (showfunc)
+		fprintf(stderr, "%s: ", func);
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	if (suffix != NULL)
+		fprintf(stderr, ": %s", suffix);
+	fputc('\n', stderr);
+	_exit(255);
+}
